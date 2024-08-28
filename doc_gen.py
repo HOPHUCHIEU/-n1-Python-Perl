4c2c097b64757b31f9a3ae39d531c247fe1e7381
HO PHUC HIEU <hophuchiei135@gmail.com>
2024-08-28 20:07:02 +0700

    lan2

---
+++ doc_gen.py
@@ -0,0 +1,18 @@
+from docxtpl import DocxTemplate
+
+doc = DocxTemplate("invoice_template.docx")
+
+invoice_list = [[2, "pen", 0.5, 1],
+                [3, "pencil", 0.25, 2],
+                [4, "eraser", 0.75, 3],
+                [5, "ruler", 1.00, 4]]
+
+doc.render({"name":"john",
+            "phone":"999999999",
+            "invoice_list": invoice_list,
+            "subtotal": 10,
+            "salestax":"10%",
+            "total":9})
+
+
+doc.save("new_invoice.docx")
